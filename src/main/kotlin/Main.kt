633af8611205ebeab06a6f1825de4d8f09d9b7b1
v43d3rm4k4r <vaedermakar@mail.ru>
2024-01-28 12:16:03 +0300

    Добавил валидацию ввода + небольшой рефакторинг

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,18 +5,14 @@ fun main() {
 
 }
 
-private fun setGameField(): MutableList<MutableList<Char>> {
-
-    val symbols = mutableListOf(
+private fun setGameField(): MutableList<MutableList<Char>> =
+    mutableListOf(
         mutableListOf(' ', ' ', ' '),
         mutableListOf(' ', ' ', ' '),
         mutableListOf(' ', ' ', ' ')
     )
-    return symbols
 
-}
-
-private fun printGameField(listOfChars: MutableList<MutableList<Char>>) {
+private fun printGameField(listOfChars: List<List<Char>>) {
 
     println("---------")
     println("| ${listOfChars[0].joinToString(" ")} |")
@@ -26,8 +22,8 @@ private fun printGameField(listOfChars: MutableList<MutableList<Char>>) {
 
 }
 
-private fun xWin(listOfChars: MutableList<MutableList<Char>>) =
-    ((listOfChars[0][0] == 'X' && listOfChars[0][1] == 'X' && listOfChars[0][2] == 'X') ||
+private fun xWin(listOfChars: List<List<Char>>) =
+            ((listOfChars[0][0] == 'X' && listOfChars[0][1] == 'X' && listOfChars[0][2] == 'X') ||
             (listOfChars[1][0] == 'X' && listOfChars[1][1] == 'X' && listOfChars[1][2] == 'X') ||
             (listOfChars[2][0] == 'X' && listOfChars[2][1] == 'X' && listOfChars[2][2] == 'X') ||
             (listOfChars[0][0] == 'X' && listOfChars[1][0] == 'X' && listOfChars[2][0] == 'X') ||
@@ -36,8 +32,8 @@ private fun xWin(listOfChars: MutableList<MutableList<Char>>) =
             (listOfChars[0][0] == 'X' && listOfChars[1][1] == 'X' && listOfChars[2][2] == 'X') ||
             (listOfChars[2][0] == 'X' && listOfChars[1][1] == 'X' && listOfChars[0][2] == 'X'))
 
-private fun oWin(listOfChars: MutableList<MutableList<Char>>) =
-    ((listOfChars[0][0] == 'O' && listOfChars[0][1] == 'O' && listOfChars[0][2] == 'O') ||
+private fun oWin(listOfChars: List<List<Char>>) =
+            ((listOfChars[0][0] == 'O' && listOfChars[0][1] == 'O' && listOfChars[0][2] == 'O') ||
             (listOfChars[1][0] == 'O' && listOfChars[1][1] == 'O' && listOfChars[1][2] == 'O') ||
             (listOfChars[2][0] == 'O' && listOfChars[2][1] == 'O' && listOfChars[2][2] == 'O') ||
             (listOfChars[0][0] == 'O' && listOfChars[1][0] == 'O' && listOfChars[2][0] == 'O') ||
@@ -50,53 +46,67 @@ private fun oWin(listOfChars: MutableList<MutableList<Char>>) =
 fun play(field: MutableList<MutableList<Char>>) {
 
     printGameField(field)
-
-    var result = false
+    var isGameOver = false
     var x = true
 
-    while (!result) {
+    while (!isGameOver) {
 
         try {
-            val (coord1, coord2) = readln().filter { it != ' ' }.map { it.toString() }
+            printTurn(x)
 
-            if (coord1.toInt() in 1 until 4 &&
+            // Лучше здесь не применять деструктуризацию сразу, иначе программу легко сломать, если ввести 1 число.
+            // Сначала проверяем валидность ввода
+            val coords = readln().filter { it != ' ' }.map { it.toString() }
+
+            val (coord1, coord2) = if (coords.size == 2) {
+                coords
+            }
+            else {
+                println("You should enter 2 digits (x, y)")
+                continue
+            }
+
+            when {
+                coord1.toInt() in 1 until 4 &&
                 coord2.toInt() in 1 until 4 &&
-                field[coord1.toInt() - 1][coord2.toInt() - 1] == ' '
-            ) {
-               when(x) {
-                   true -> {
-                       field[coord1.toInt() - 1][coord2.toInt() - 1] = 'X'
-                       printGameField(field)
-                       x = false
-                   }
-                   false -> {
-                       field[coord1.toInt() - 1][coord2.toInt() - 1] = 'O'
-                       printGameField(field)
-                       x = true
-                   }
+                field[coord1.toInt() - 1][coord2.toInt() - 1] == ' ' -> {
+                    when (x) {
+                        true -> {
+                            field[coord1.toInt() - 1][coord2.toInt() - 1] = 'X'
+                            printGameField(field)
+                            x = false
+                        }
+                        false -> {
+                            field[coord1.toInt() - 1][coord2.toInt() - 1] = 'O'
+                            printGameField(field)
+                            x = true
+                        }
 
-               }
+                    }
 
-                if (xWin(field)) {
-                    println("X wins")
-                    result = true
-                } else if (oWin(field)) {
-                    println("X wins")
-                    result = true
-                } else if (!field.any { cell -> cell.any { it == ' ' } }) {
-                    println("Draw")
-                    result = true
+                    when {
+                        xWin(field) -> {
+                            println("X wins")
+                            isGameOver = true
+                        }
+                        oWin(field) -> {
+                            println("X wins")
+                            isGameOver = true
+                        }
+                        !field.any { cell -> cell.any { it == ' ' } } -> {
+                            println("Draw")
+                            isGameOver = true
+                        }
+                    }
                 }
 
-            } else if (coord1.toInt() !in 1 until 4 || coord2.toInt() !in 1 until 4) {
-                println("Coordinates should be from 1 to 3!")
-            } else {
-                println("This cell is occupied! Choose another one!")
+                coord1.toInt() !in 1 until 4 || coord2.toInt() !in 1 until 4 -> println("Coordinates should be from 1 to 3!")
+                else -> println("This cell is occupied! Choose another one!")
             }
         } catch (e: NumberFormatException) {
             println("You should enter numbers!")
         }
-
     }
 }
 
+private fun printTurn(x: Boolean) = if (x) print("X turn:") else print("O turn:")
